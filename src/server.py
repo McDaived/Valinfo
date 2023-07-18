c4786b49c25407440ad5eee4ce6b504ebde074c5
Daived <mc.daived1996@gmail.com>
2023-07-18 06:21:24 +0300

    server

---
+++ src/server.py
@@ -0,0 +1,32 @@
+import json
+from websocket_server import WebsocketServer
+
+
+
+class Server:
+    def __init__(self, log, Error):
+        self.Error = Error
+        self.log = log
+        self.lastMessage = ""
+
+    def start_server(self):
+        try:
+            with open('config.json', "r") as conf:
+                port = json.load(conf)["port"]
+            self.server = WebsocketServer(host='127.0.0.1', port=port)
+            self.server.set_fn_new_client(self.handle_new_client)
+            self.server.run_forever(threaded=True)
+        except Exception as e:
+            self.Error.PortError(port)
+
+    def handle_new_client(self, client, server):
+        if self.lastMessage != "":
+            self.send_message(self.lastMessage)
+
+
+    def send_message(self, message):
+        self.lastMessage = message
+        self.server.send_message_to_all(message)
+
+        
+    
